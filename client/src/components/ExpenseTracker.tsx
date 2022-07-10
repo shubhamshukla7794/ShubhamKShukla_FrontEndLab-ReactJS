071c460bb14125b5adc65e5d87b686a74c593377
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2022-07-10 13:04:02 +0530

    Added Modal

--- client/src/components/ExpenseTracker.tsx
+++ client/src/components/ExpenseTracker.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import {Container, Spinner, Alert, Table } from 'react-bootstrap';
+import {Container, Spinner, Alert, Table, Button, Modal } from 'react-bootstrap';
 import IItem from "../models/IItem";
 import { getItems } from "../services/items";
 
@@ -9,6 +9,7 @@ const ExpenseTracker = () => {
   const [ items, setItems ] = useState<IItem[]>([] as IItem[]);
   const [ error, setError ] = useState<Error | null>( null );
   const [ loading, setLoading ] = useState<boolean>( true );
+  const [show, setShow] = useState(false);
 
   useEffect( () => {
     const fetchItems = async () => {
@@ -39,9 +40,56 @@ const ExpenseTracker = () => {
     return total;
   };
 
+
+  const differncePayment = () => {
+    let difference = 0;
+    let rahulAmt = 0;
+    let rameshAmt = 0;
+    let payee = '';
+
+    for(let i = 0; i < items.length; i++) {
+        if (items[i].payeeName === 'Rahul') {
+            rahulAmt += items[i].price;
+        } else {
+            rameshAmt += items[i].price;
+        }
+    }
+
+    if (rahulAmt > rameshAmt) {
+        difference = (rahulAmt - rameshAmt) / 2 ;
+        payee = 'Ramesh'
+    } else {
+        difference = (rameshAmt - rahulAmt) / 2 ;
+        payee = 'Rahul'
+    }
+        return { difference , payee};
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
     return (
-        <Container>
-        <h1>Expense Tracker</h1>
+        <Container className="my-4">
+        <h1>
+            Expense Tracker
+            <Button variant="primary" onClick={handleShow}className="float-end">Add Expense</Button>
+        </h1>
+
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+        
         {
             loading && (
                 <div className="d-flex justify-content-center">
@@ -61,7 +109,7 @@ const ExpenseTracker = () => {
 <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
+          <th>Sr. No.</th>
           <th>Payee</th>
           <th>Description</th>
           <th>Date</th>
